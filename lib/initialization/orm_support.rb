837cc312a54195ef6afdd46edc0ebb7169545374
Mark Bates <mark@mackframework.com>
2008-06-16 22:40:11 -0400

    Added a begin/rescue for orm_support

--- lib/initialization/orm_support.rb
+++ lib/initialization/orm_support.rb
@@ -1,6 +1,10 @@
 orm = app_config.orm || 'data_mapper'
 unless orm.nil? 
   Mack.logger.info "Initializing #{orm} orm..."
-  require "mack-#{orm}"
-  require "mack-#{orm}_tasks"
+  begin
+    require "mack-#{orm}"
+	  require "mack-#{orm}_tasks"
+  rescue Exception => e
+    Mack.logger.warn "There was an error configuring #{orm}"
+  end
 end
