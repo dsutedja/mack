f08ea00bb2b5f1254d32a5a45221c2a487e64ef0
dsutedja <dsutedja@helium.com>
2008-06-18 11:25:19 -0400

    - ORM will not be initialized if the app explicitly says it doesn't want to use any ORM. - Updated generated task test to reflect this new change. - Updated the mack_gem_task to use latest version of the mack_ruby_core_extensions (0.2.0)--although, we may need to make it use the facets anyway...

--- test/lib/tasks/generator_tasks_test.rb
+++ test/lib/tasks/generator_tasks_test.rb
@@ -5,24 +5,44 @@ class GeneratorTasksTest < Test::Unit::TestCase
   def test_list
     rake_task("generator:list") do
       assert_not_nil ENV["__generator_list"]
-      list = <<-LIST
+      
+      model_gen_str = scaffold_gen_str = migration_gen_str = ""
+      
+      if !app_config.orm.nil?
+        list = <<-LIST
 
 Available Generators:
 
 MackApplicationGenerator
 	rake generate:mack_application_generator
-MigrationGenerator
-	rake generate:migration_generator
 ModelGenerator
 	rake generate:model_generator
-PluginGenerator
-	rake generate:plugin_generator
 ScaffoldGenerator
 	rake generate:scaffold_generator
+PluginGenerator
+	rake generate:plugin_generator
+MigrationGenerator
+	rake generate:migration_generator
+        LIST
+      else
+        list = <<-LIST
 
+Available Generators:
 
-LIST
-      assert_equal list, ENV["__generator_list"]
+MackApplicationGenerator
+	rake generate:mack_application_generator
+PluginGenerator
+	rake generate:plugin_generator
+        LIST
+      end
+      
+      # puts "---------------------------------"
+      # puts "TEST: \n#{list}"
+      # puts
+      # puts ENV["__generator_list"]
+      # puts "---------------------------------"
+      
+      assert_equal list.squeeze, ENV["__generator_list"].squeeze
     end
   end
   
